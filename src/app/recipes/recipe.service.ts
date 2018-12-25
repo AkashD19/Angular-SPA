d962a4832389ea8ae6ef90df529e6c2ff6d33bf6
Akash Dutta <akash.dutta@hoonuit.com>
2018-12-25 00:02:18 -0600

    FirstCommit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,40 @@
+import {Recipe} from './recipes.model';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'TestRecipe',
+      'This is just a test.',
+      'https://cdn.cnn.com/cnnnext/dam/assets/171027052520-processed-foods-exlarge-tease.jpg',
+      [
+        new Ingredient('Meat',1),
+        new Ingredient('Potatoes',20)
+      ]),
+    new Recipe('TestRecipe2',
+      ' another test',
+      'https://www.cbc.ca/food/content/images/recipes/Stuffing.jpg',
+      [
+        new Ingredient('Buns',2),
+        new Ingredient('Meat',1)
+      ])
+  ];
+  recipeSelected = new EventEmitter<Recipe>();
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+
+  }
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
+
+}
---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,21 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') namedInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit() {
+  }
+  onAddItem() {
+    const newIngredient = new Ingredient(this.namedInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
+    this.slService.addIngredient(newIngredient);
+  }
+}
