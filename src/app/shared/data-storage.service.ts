32450a90b50fce4b348a234a61c0deab448b2717
Akash Dutta <akash.dutta@hoonuit.com>
2019-01-13 19:51:12 -0600

    Authentication Update.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,22 +1,26 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {RecipeService} from '../recipes/recipe.service';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 
 export class DataStorageService {
   constructor (private http: HttpClient,
-               private recipeService: RecipeService) {}
+               private recipeService: RecipeService,
+               private authService: AuthService) {}
 
   storeRecipes() {
-    return this.http.put('https://ng-project-44358.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+    return this.http.put('https://ng-project-44358.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://ng-project-44358.firebaseio.com/recipes.json')
+    const token = this.authService.getToken();
+    this.http.get('https://ng-project-44358.firebaseio.com/recipes.json?auth=' + token)
       .subscribe(
-        (data) => {
-          for (const recipe of data) {
+        (data: any[]) => {
+          for (let recipe of data) {
             if (!recipe['ingredients']) {
               console.log(recipe);
               recipe['ingredients'] = [];
